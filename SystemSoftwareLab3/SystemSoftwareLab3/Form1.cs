90d3cf6e776c54e58ee74d002e5d52ad6920a36d
Dimitrij Kozlov <ikzdimitrij@gmail.com>
2015-12-07 05:37:00 +0200

    Bug fixing

--- SystemSoftwareLab3/SystemSoftwareLab3/Form1.cs
+++ SystemSoftwareLab3/SystemSoftwareLab3/Form1.cs
@@ -7,8 +7,8 @@ namespace _1SPZv1
 {
     public partial class Form1 : Form
     {
-        List<Task> tasks = new List<Task>();
-        List<int> filo = new List<int>();
+        readonly List<Task> _tasks = new List<Task>();
+        readonly List<int> _filo = new List<int>();
         int _processingTask = -1;
         int _currentTick;
 
@@ -34,26 +34,26 @@ namespace _1SPZv1
 
             if (rand.Next(100) < Convert.ToInt32(label2.Text))
             {
-                filo.Add(tasks.Count);
-                tasks.Add(new Task(_currentTick));
+                _filo.Add(_tasks.Count);
+                _tasks.Add(new Task(_currentTick));
             }
 
             if (_processingTask == -1)
-                if (filo.Count == 0)
+                if (_filo.Count == 0)
                     return;
                 else
                 {
-                    _processingTask = filo.Last();
-                    filo.Remove(_processingTask);
+                    _processingTask = _filo.Last();
+                    _filo.Remove(_processingTask);
                 }
 
-            if (tasks[_processingTask].Processing(_currentTick))
+            if (_tasks[_processingTask].Processing(_currentTick))
             {
                 _processingTask = -1;
-                if (filo.Count != 0)
+                if (_filo.Count != 0)
                 {
-                    _processingTask = filo.Last();
-                    filo.Remove(_processingTask);
+                    _processingTask = _filo.Last();
+                    _filo.Remove(_processingTask);
                 }
             }
 
@@ -66,36 +66,36 @@ namespace _1SPZv1
         {
             var waitTime = 0;
             listView1.Items.Clear();
-            for (var i = 0; i < tasks.Count; i++)
+            for (var i = 0; i < _tasks.Count; i++)
             {
-                waitTime += (tasks[i].GetFinishTick() == -1)
-                    ? (_currentTick - tasks[i].GetCreateTick())
-                    : tasks[i].GetFinishTick() - tasks[i].GetCreateTick() - tasks[i].GetWorkTime();
+                waitTime += (_tasks[i].GetFinishTick() == -1)
+                    ? (_currentTick - _tasks[i].GetCreateTick())
+                    : _tasks[i].GetFinishTick() - _tasks[i].GetCreateTick() - _tasks[i].GetWorkTime();
                 label9.Text = (_processingTask != -1)? _processingTask.ToString(): "*";
                 listView1.Items.Add(i.ToString());
-                listView1.Items[i].SubItems.Add(tasks[i].GetCreateTick().ToString());
-                listView1.Items[i].SubItems.Add(tasks[i].GetWorkTime().ToString());
-                listView1.Items[i].SubItems.Add(((tasks[i].GetFinishTick() == -1)? (_currentTick - tasks[i].GetCreateTick()): tasks[i].GetFinishTick() - tasks[i].GetCreateTick() - tasks[i].GetWorkTime()).ToString());
-                listView1.Items[i].SubItems.Add((tasks[i].GetFinishTick() == -1)? "Not finished yet": tasks[i].GetFinishTick().ToString());
-                listView1.Items[i].SubItems.Add(tasks[i].IsCoplited()? "Completed": (i == _currentTick)? "Working": "Waiting");
+                listView1.Items[i].SubItems.Add(_tasks[i].GetCreateTick().ToString());
+                listView1.Items[i].SubItems.Add(_tasks[i].GetWorkTime().ToString());
+                listView1.Items[i].SubItems.Add(((_tasks[i].GetFinishTick() == -1)? (_currentTick - _tasks[i].GetCreateTick()): _tasks[i].GetFinishTick() - _tasks[i].GetCreateTick() - _tasks[i].GetWorkTime()).ToString());
+                listView1.Items[i].SubItems.Add((_tasks[i].GetFinishTick() == -1)? "Not finished yet": _tasks[i].GetFinishTick().ToString());
+                listView1.Items[i].SubItems.Add(_tasks[i].IsCoplited()? "Completed": (i == _currentTick)? "Working": "Waiting");
                 listView1.EnsureVisible(i);
             }
-            label5.Text = (tasks.Count == 0? 0: waitTime / tasks.Count).ToString() + " ms";
-            label3.Text = filo.Count.ToString();
+            label5.Text = (_tasks.Count == 0? 0: waitTime / _tasks.Count).ToString() + @" ms";
+            label3.Text = _filo.Count.ToString();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             timer1.Enabled = false;
-            filo.Clear();
-            tasks.Clear();
+            _filo.Clear();
+            _tasks.Clear();
             listView1.Items.Clear();
             _processingTask = -1;
             _currentTick = 0;
-            label3.Text = "0";
-            label5.Text = "0";
-            label8.Text = "0";
-            label9.Text = "0";
+            label3.Text = @"0";
+            label5.Text = @"0";
+            label8.Text = @"0";
+            label9.Text = @"0";
         }
     }
 }
